9593072c9a821575c22226d612a0d50384086cd6
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-13 12:07:31 +0200

    final commit to IWA14.2

--- challenge2/scripts.js
+++ challenge2/scripts.js
@@ -1,38 +1,42 @@
-// script.js
+function add(a, b) {
+    return a + b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function internal() {
+    const added = this.add(this.internal.a, this.internal.b);
+    const multiplied = this.multiply(this.internal.a, this.internal.b);
+    console.log(added * this.internal.c);
+    return this;
+  }
 
-function add = a, b => { a + b }
-
-function multiply = a, b => { a - b }
-
-function internal() {
-	const added = this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return this
-}
-
-// Not allowed to change below this
-
-const example1 = {
-	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
-	},
-	add,
-	multiply,
-  calculate: internal
-}
-
-const example2 = {
-	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
-	},
-	add,
-	multiply,
-  calculate: internal
-}
-
-example1.calculate()
-example2.calculate()
+  // Not allowed to change below this
+  
+  const example1 = {
+    internal: {
+      a: 2,
+      b: 4,
+      c: 8,
+    },
+    add,
+    multiply,
+    calculate: internal
+  };
+  
+  const example2 = {
+    internal: {
+      a: 2,
+      b: 2,
+      c: 3,
+    },
+    add,
+    multiply,
+    calculate: internal
+  };
+  
+  example1.calculate();
+  example2.calculate();
+  
