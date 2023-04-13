94b6ec20e0f8807fa8b19f4e8916a6bd8b6c0c17
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-13 11:46:16 +0200

    final commit to IWA14.1

--- challenge1/scripts.js
+++ challenge1/scripts.js
@@ -1,14 +1,14 @@
-firstName = 'John';
-age = 35;
-hobby = 'Coding';
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
 
-const logTwice = () => {
-  console(parameter)
-  console(parameter)
+const logTwice = (parameter) => {
+  console.log(parameter);
+  console.log(parameter);
 }
 
-function hobby () {
-  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+const sayHello = ()  =>{
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
 }
 
-hobby()
+sayHello();
